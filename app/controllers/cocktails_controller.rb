b93083895a67d2b3eca256b05f4da3cf5ee49cce
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-07 17:41:37 +0100

    m

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,29 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.all
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+    @ingredients = Ingredient.all
+  end
+
+  def create
+@cocktail = Cocktail.new(cocktail_params)
+
+    respond_to do |format|
+      if @cocktail.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+        format.json { render :show, status: :created, location: @cocktail }
+      else
+        format.html { render :new }
+        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :cocktail, only: [:index, :show, :new, :create] do
+    resources :dose, only: [:new, :create]
+  end
+  resources :dose, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
